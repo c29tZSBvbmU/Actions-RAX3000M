3151587fb40fe3b9d17cdbfd56b99ec6ae9ecfc1
c29tZSBvbmU <86731433+c29tZSBvbmU@users.noreply.github.com>
2024-11-25 10:59:29 +0800

    Update diy-part2.sh

--- close-xr30/diy-part2.sh
+++ close-xr30/diy-part2.sh
@@ -13,7 +13,7 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 #sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把 RAX3000M 修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i 's/ImmortalWrt/XR30-nand/g' package/base-files/files/bin/config_generate
+#sed -i 's/ImmortalWrt/XR30-nand/g' package/base-files/files/bin/config_generate
 
 # Enable wifi
 # sed -i 's/.disabled=1/.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
